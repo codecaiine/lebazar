4bb759e12ddb2847c26364c991292db19c8cc1f8
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-03-18 21:44:56 +0000

    Create user and category models

--- category/models.py
+++ category/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, unique=True)
+    slug = models.CharField(max_length=100, unique=True)
+    description = models.TextField(max_length=255, blank=True)
+    category_image = models.ImageField(upload='photos/categories', blank=True)
