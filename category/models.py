80358123083c22a85e80896b83acd3e929435e86
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-03-18 22:05:38 +0000

    Migrate category

--- category/models.py
+++ category/models.py
@@ -5,7 +5,8 @@ class Category(models.Model):
     category_name = models.CharField(max_length=50, unique=True)
     slug = models.CharField(max_length=100, unique=True)
     description = models.TextField(max_length=255, blank=True)
-    category_image = models.ImageField(upload='photos/categories', blank=True)
+    category_image = models.ImageField(upload_to='photos/categories', blank=True)
+
 
     def __str__(self):
         return self.category_name 
